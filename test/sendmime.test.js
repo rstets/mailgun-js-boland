53613fc57808c2328b78f55034d46640208e9f79
Roman Stets <rstets@rebbix.com>
2021-09-27 12:51:47 +0300

    NPM audit fixes #2

--- test/sendmime.test.js
+++ test/sendmime.test.js
@@ -3,7 +3,6 @@ const fixture = require('./data/fixture.json')
 const assert = require('assert')
 const fs = require('fs')
 const path = require('path')
-const MailComposer = require('nodemailer/lib/mail-composer')
 
 const mailgun = require('../')({
   'apiKey': auth.api_key,
@@ -13,64 +12,6 @@ const mailgun = require('../')({
 describe('Send MIME', () => {
   beforeEach((done) => {
     setTimeout(done, 500)
-  })
-
-  it('test sendMime()', (done) => {
-    const mailOptions = {
-      'from': fixture.message.from,
-      'to': fixture.message.to,
-      'subject': fixture.message.subject,
-      'body': fixture.message.text,
-      'html': `<b>${fixture.message.text}</b>`
-    }
-
-    const mail = new MailComposer(mailOptions)
-
-    mail.compile().build((err, message) => {
-      assert.ifError(err)
-
-      const data = {
-        'to': fixture.message.to,
-        'message': message.toString('ascii')
-      }
-
-      mailgun.messages().sendMime(data, (err, body) => {
-        assert.ifError(err)
-        assert.ok(body.id)
-        assert.ok(body.message)
-        assert(/Queued. Thank you./.test(body.message))
-        done()
-      })
-    })
-  })
-
-  it('test sendMime() with to field as array', (done) => {
-    const mailOptions = {
-      'from': fixture.message.from,
-      'to': fixture.message.to,
-      'subject': fixture.message.subject,
-      'body': fixture.message.text,
-      'html': `<b>${fixture.message.text}</b>`
-    }
-
-    const mail = new MailComposer(mailOptions)
-
-    mail.compile().build((err, message) => {
-      assert.ifError(err)
-
-      const data = {
-        'to': fixture.message_recipient_vars.to,
-        'message': message.toString('ascii')
-      }
-
-      mailgun.messages().sendMime(data, (err, body) => {
-        assert.ifError(err)
-        assert.ok(body.id)
-        assert.ok(body.message)
-        assert(/Queued. Thank you./.test(body.message))
-        done()
-      })
-    })
   })
 
   it('test sendMime() with file path', (done) => {
